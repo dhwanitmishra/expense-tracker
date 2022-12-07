4d9d99a8f036dcd413770a6a43490fd0ce5329d2
dhwanitmishra <dhvanitmisra@gmail.com>
2022-12-07 16:07:02 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import {ExpenseItem} from "./Components/ExpenseItem";
+
+function App() {
+  return (
+    <div>
+      <h2>
+        Let's Get Started
+      </h2>
+      <p>This is also visible!</p>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
