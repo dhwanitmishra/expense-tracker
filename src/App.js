33258074419efebcee5ae2e8ce21febb6cef8ee7
dhwanitmishra <dhvanitmisra@gmail.com>
2022-12-09 17:18:30 +0530

    Fix: Container around expenses

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { ExpenseItem } from "./Components/ExpenseItem";
+import { Expenses } from "./Components/Expenses";
+
 function App() {
   const expenses = [
     { title: 'Car Insurance', amount: 10000, date: new Date(2022, 11, 1) },
@@ -10,11 +11,8 @@ function App() {
   return (
     <div>
       <h2>Expense Tracker</h2>
-      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
-      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
-      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItem>
-      <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}></ExpenseItem>
-    </div>
+      <Expenses items={expenses} />
+      </div>
   );
 }
 
