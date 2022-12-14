b476fe04a51a3c5a6736e4c1fa3d3cf5159b54d3
dhwanitmishra <dhvanitmisra@gmail.com>
2022-12-14 21:43:59 +0530

    added dynamic lists

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
+import React, { useState } from 'react';
 import { Expenses } from "./Components/Expenses/Expenses";
 import { NewExpense } from "./Components/NewExpense/NewExpense";
 
 function App() {
-  const expenses = [
+  const DUMMY_EXPENSES = [
     { title: 'Car Insurance', amount: 10000, date: new Date(2022, 11, 1) },
     { title: 'Groceries', amount: 1000, date: new Date(2022, 11, 7) },
     { title: 'Fuel', amount: 500, date: new Date(2022, 11, 10) },
     { title: 'Clothes', amount: 200, date: new Date(2022, 11, 12) },
   ];
 
+const [expenses, setExpenses]= useState(DUMMY_EXPENSES)
+
   const addExpenseHandler = (expense) => {
-    console.log('In App.js');
-    console.log(expense);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   }
 
   return (
